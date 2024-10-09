7bab082fb162bc22c12f8f1ad06a62667696ac3b
Muhammad Arif <arifluqman477@gmail.com>
2024-10-10 04:12:17 +0500

    removed

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,14 +4,14 @@ import Banner from "@/app/public/about.png"
 export default function About() {
     return (
         <section className="lg:container mx-auto lg:flex lg:justify-between lg:items-center p-2">
-            <div className="lg:w-1/2 w-full flex justify-center">
+            <div className="lg:w-1/2 w-full flex">
                 <Image src={Banner} alt="about image" width={300} height={300} />
             </div>
             <div className="lg:w-1/2 w-full ">
                 <h2 className="text-3xl mt-10 mb-4 lg:mt-0">About <span className="font-bold">Me</span> </h2>
-                <p>I&apos;m a passionate&lsquo; self-proclaimed designer who specializes in full stack development (React.js & Node.js). I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience&lsquo; pixel perfect design&lsquo; and writing clear&lsquo; readable&lsquo; highly performant code matters to me.
-                    I began my journey as a web developer in 2022&lsquo; and since then&lsquo; I&apos;ve continued to grow and evolve as a developer&lsquo; taking on new challenges and learning the latest technologies along the way. Now&lsquo; in my early thirties&lsquo; 2 years after starting my web development journey&lsquo; I&apos;m building cutting-edge web applications using modern technologies such as Next.js&lsquo; TypeScript&lsquo; Tailwindcss and much more.
-                    You can follow me on Twitter where I share tech-related bites and build in public&lsquo; or you can follow me on GitHub.</p>
+                <p>I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me.
+                    I began my journey as a web developer in 2022, and since then, I've continued to grow and evolve as a developer, taking on new challenges and learning the latest technologies along the way. Now, in my early thirties, 2 years after starting my web development journey, I'm building cutting-edge web applications using modern technologies such as Next.js, TypeScript, Tailwindcss and much more.
+                    You can follow me on Twitter where I share tech-related bites and build in public, or you can follow me on GitHub.</p>
             </div>
         </section>
     )
--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -3,10 +3,11 @@ import img from "@/app/public/Banner.png";
 import { FaTwitter, FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { IoLogoWhatsapp } from "react-icons/io";
 import Link from "next/link";
+// components/Hero.js
 const HeroSection = () => {
     return (
         <section className="lg:container mx-auto lg:w-full lg:h-screen flex flex-col-reverse lg:flex lg:flex-row lg:justify-between lg:items-center">
-            <div className="lg:w-1/2 w-full lg:h-screen lg:flex lg:justify-center lg:items-center p-2">
+            <div className="lg:w-1/2 w-full lg:h-screen lg:flex lg:justify-center lg:items-center bg-slate-50 p-2">
                 <div className="lg:w-[90%]">
                     <h1 className="lg:w-3/4 lg:text-4xl text-3xl lg:leading-[4rem] leading-10 font-light ">
                         Hello I am
@@ -16,7 +17,7 @@ const HeroSection = () => {
                         Based in
                         <span className="font-bold">Pakistan</span>
                     </h1>
-                    <p className="text-[12px]">I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s&lsquo; when an unknown printer took a galley of type and scrambled it to specimen book.</p>
+                    <p className="text-[12px]">I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.</p>
                     <div className="flex space-x-4 my-4">
                         <Link className=" border border-black rounded p-2" href="https://linkedin.com">
                             <FaGithub />
@@ -36,7 +37,7 @@ const HeroSection = () => {
                     </div>
                 </div>
             </div>
-            <div className="lg:w-1/2 w-full lg:h-screen p-2 my-4 flex justify-center items-center">
+            <div className="lg:w-1/2 w-full lg:h-screen p-2 my flex justify-center items-center">
                 <Image className=" " src={img} alt="image" width={400} />
             </div>
 
